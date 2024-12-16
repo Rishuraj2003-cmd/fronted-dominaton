de75e008ac426f72828434f5fce1416158344ae8
RISHU RAJ <RR907194@GMAIL.com>
2024-12-16 21:39:57 +0530

    new file:   FInal Proj/apple-touch-icon.png 	modified:   FInal Proj/index.html     modified:   FInal Proj/script.js     modified:   FInal Proj/style.css

--- FInal Proj/script.js
+++ FInal Proj/script.js
@@ -2,7 +2,7 @@ var tl = gsap.timeline();
 tl.from(".line h1", {
   y: 150,
   stagger: 0.25,
-  duration: 0.6,
+  duration: 0.3,
   delay: 0.5,
 });
 tl.from("#line1-part1", {
@@ -11,22 +11,27 @@ tl.from("#line1-part1", {
     var h5timer = document.querySelector("#line1-part1 h5");
     var grow = 0;
     setInterval(function () {
-      if (grow < 100) {
+      if (grow <= 100) {
         h5timer.innerHTML = grow++;
       } else {
-        h5timer.innerHTML = grow;
+        h5timer.innerHTML = 100;
       }
-    }, 33);
+    }, 21);
   },
 });
-tl.to(".line h2", {
-  animatioName: "anime",
-  opacity: 1,
-});
+
+tl.to(".blink h2", {
+    opacity: 0,
+    duaration: 2,
+    animationName: "anime",
+    duaration: 0.2,
+  });
+
 tl.to("#loader", {
   opacity: 0,
   delay: 4,
   duration: 0.2,
+  stagger:0.1,
 });
 tl.from("#page1", {
   opacity: 0,
